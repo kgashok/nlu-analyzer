32dfda6c4446ecd83b1434fe38e10c7dd19fad94
Ashok B <lifebalance@gmail.com>
2023-07-31 01:04:56 +0530

    keyboard not handled yet

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import keyboard
 if __name__ == '__main__':
     app.run()
 
+    # This doesn't work, obviously 
     while True: 
         if keyboard.read_key() == "n": 
             print("Generate new BST") 
