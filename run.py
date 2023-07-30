4b7658668acf58a7d9047a30d38b8947c0d6a8a0
kgashok <kgashok@users.noreply.github.com>
2023-07-30 18:45:36 +0000

    added keyboard module

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import app
+import keyboard
 
 if __name__ == '__main__':
     app.run()
+
+    while True: 
+        if keyboard.read_key() == "n": 
+            print("Generate new BST") 
+            app.run()
