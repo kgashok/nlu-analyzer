c948a4453516a2ed8685eeed5d60dda1a0c48668
kgashok <kgashok@users.noreply.github.com>
2023-08-01 15:35:55 +0000

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,49 @@
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
-from binarytree import Node, tree
-from app.bst import BST
+# First we need the CORS thinymajig
+from flask_cors import CORS, cross_origin
 import random
 import time 
 
-app = Flask(__name__)
-api = Api(app)
+import json
+import time
+#from datetime import datetime
+import random
 
-# The sample list to be used to generate the BST 
-# in case the user specifies range=true on the URL
-biglist = list(range(1, 30))
+import requests
+from bs4 import BeautifulSoup as bs
+
+from ibm_watson import NaturalLanguageUnderstandingV1, ApiException
+#from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, CategoriesOptions, 
+from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOptions, SummarizationOptions
+#from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+# Authentication via IAM
+# authenticator = IAMAuthenticator('your_api_key')
+# service = NaturalLanguageUnderstandingV1(
+#     version='2018-03-16',
+#     authenticator=authenticator)
+# service.set_service_url('https://gateway.watsonplatform.net/natural-language-understanding/api')
+
+# Authentication via external config like VCAP_SERVICES
+service = NaturalLanguageUnderstandingV1(
+    version='2018-03-16')
+#service.set_service_url('https://gateway.watsonplatform.net/natural-language-understanding/api')
+
+#response = service.analyze(
+#    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+#    'Superman fears not Banner, but Wayne.',
+#    features=Features(entities=EntitiesOptions(),
+#                      keywords=KeywordsOptions())).get_result()
+
+#print(json.dumps(response, indent=2))
+
+
+# Put this right after you declare the app
+
+app = Flask(__name__)
+cors = CORS(app)
+api = Api(app)
 
 class MainResource(Resource):
     """handles the parsing of the URL for node information
@@ -19,62 +52,60 @@ class MainResource(Resource):
         Resource (_type_): _description_
     """    
     def get(self):
-        """parses the URL to build a BST and generate 
-        a string equivalent of the same for display
+        nlu_url = request.args.get('url')
+        print("nlu_url", nlu_url)
 
-        Returns:
-           Response: containing the string version of the BST object
-           See https://flask.palletsprojects.com/en/2.3.x/api/#flask.Flask.make_response
-           and https://flask.palletsprojects.com/en/2.3.x/api/#flask.Response
- 
-        """        
-        # process the query params
-        balanced = None
-        nodes = list()
-        shuffle = "true"
-        range = request.args.get('range')
-        shuffle = request.args.get('shuffle')
-        n = request.args.get('nodes')
+        nlu_url = nlu_url.strip()
+        response = None
+        
+        try:
+            response = service.analyze(
+                return_analyzed_text="true", url=nlu_url,
+                features=Features(metadata={}, categories=CategoriesOptions(), summarization= SummarizationOptions() \
+                                #entities=EntitiesOptions(), \
+                                #keywords=KeywordsOptions() \
+                                )).get_result()
+            '''response = service.analyze(
+                url=nlu_url,
+                features=Features(categories=CategoriesOptions(), \
+                                #entities=EntitiesOptions(), \
+                                #keywords=KeywordsOptions() \
+                                )).get_result()
+            '''
+            print("***nlu success!")
             
-        if n:
-            n = n.strip().split(',')
-            try: 
-                nodes = [int(e) for e in n]
-                # print(nodes, nodes[0], nodes[1])
-            except: 
-                response = make_response("An error occurred, please double check your input!", 400)
-                return response
-        else:
-            print("No node information given. Using standard list!")
-            nodes[:] = biglist 
-                            
-        if shuffle != "false": # and shuffle.lower() == 'true':
-            # do you want a https://kov.ai/linearTree?
-            linearTree = random.choice([False, True, False, False, False])
-            if linearTree:
-                nodes.sort()
-                balanced = random.choice([False, True]) 
-            else:
-                random.shuffle(nodes)
-
-        bst = BST(balanced=balanced)        
-        if not balanced: 
-            try:
-                for n in nodes:
-                    bst.binary_insert(int(n))
-            except:
-                #return 'An error occurred, please double check your input!'
-                response = make_response("Cannot generate BST. Check your input!", 400)
-                return response
-
-        response = make_response(bst.get_output(), 200)
-        response.mimetype = 'text/plain'
-                
+        except ApiException as error:
+            print(error)
+            response = make_response(error.message, error.code)
+            return response 
+            # return error.http_response
+            
+        if response: 
+            # print(json.dumps(response, indent=2))
+            ret_url = response['retrieved_url']
+            print(ret_url)
+            title = response['metadata']['title']
+            #response = make_response(nlu_url, 200)
+            #response.mimetype = 'text/json'
+            print("title from metadata", title)
+            r = requests.get(ret_url)
+            soup = bs(r.content, 'lxml')
+            #print(soup)
+            if soup and soup.select_one('title'): 
+                title2 = soup.select_one('title').text
+                print("title from soup", title2)
+                if len(title2) > len(title):
+                    response['metadata']["title"] = title2 
+        
         return response
+    
 
+api.add_resource(MainResource, '/analyze')
 
-api.add_resource(MainResource, '/')
 
 if __name__ == '__main__':
     random.seed(time)
     app.run()
+
+
+
